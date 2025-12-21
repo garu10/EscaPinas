ffeb85ddb6d6ab054a435dc266209e18c067b1d7
John Ralph Alcantara <alcantarajohnralph@gmail.com>
2025-12-22 03:24:15 +0800

    escapanis

---
+++ frontend/components/footer.php
@@ -0,0 +1,67 @@
+<div class="container-fluid fixed-bottom" style="background-color: #0CA458;">
+    <div class="container pb-4">
+        <div class="row text-center text-md-start justify-content-between">
+            
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <div class="d-flex align-items-center justify-content-center justify-content-md-start mb-3">
+                    <img src="assets/Logo 1.png" height="200" class="d-inline-block align-text-top">
+                </div>
+                <p class="small text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, ad quis? A, 
+                    fuga facere ex eaque iusto ad quis dolor? Nemo nam eos error tempora velit iste ab ipsum illo?
+                </p>
+                <div class="mt-4">
+                    <a href="#" class="text-light me-3 fs-5"><i class="bi bi-facebook"></i></a>
+                    <a href="#" class="text-white me-3 fs-5"><i class="bi bi-instagram"></i></a>
+                    <a href="#" class="text-white me-3 fs-5"><i class="bi bi-twitter-x"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold text-light">Our Services</h6>
+                <ul class="list-unstyled small lh-lg">
+                    <li><a href="tours.php?cat=adventure" class="text-light text-decoration-none hover-link">Di pa sure</a></li>
+                    <li><a href="tours.php?cat=honeymoon" class="text-light text-decoration-none hover-link">Di pa sure</a></li>
+                    <li><a href="tours.php?cat=family" class="text-light text-decoration-none hover-link">Di pa sure</a></li>
+                    <li><a href="destinations.php" class="text-light text-decoration-none hover-link">Di pa sure</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold text-light">Company</h6>
+                <ul class="list-unstyled small lh-lg">
+                    <li><a href="about.php" class="text-light text-decoration-none hover-link">About Us</a></li>
+                    <li><a href="faq.php" class="text-light text-decoration-none hover-link">Travel FAQs</a></li>
+                    <li><a href="privacy.php" class="text-light text-decoration-none hover-link">Privacy Policy</a></li>
+                    <li><a href="terms.php" class="text-light text-decoration-none hover-link">Terms & Conditions</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 fw-bold text-light">Contact Us</h6>
+                <div class="small text-light lh-lg">
+                    <p class="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
+                        <i class="bi bi-house-door me-3 fs-5"></i> Tanauan City, Batangas, PH
+                    </p>
+                    <p class="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
+                        <i class="bi bi-envelope me-3 fs-5"></i> escapinas@test.com
+                    </p>
+                    <p class="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
+                        <i class="bi bi-telephone me-3 fs-5"></i> +63 966 693 5239
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <hr class="my-4 border-secondary opacity-25">
+
+        <div class="row align-items-center">
+            <div class="col-12 text-center">
+                <p class="small text-light p-2 mb-0">
+                    © <?php echo date("Y"); ?> <strong>EscaPinas</strong>. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
