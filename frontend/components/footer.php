49e89c0706461f84f8f294207dd40142964678f1
Althea Jade Condes <altheacondes145@gmail.com>
2026-01-11 19:41:32 +0800

    Clean the footer

--- frontend/components/footer.php
+++ frontend/components/footer.php
@@ -2,17 +2,13 @@
     <div class="container pb-4">
         <div class="row text-center text-md-start justify-content-between">
             
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5">
-                <div class="d-flex align-items-center justify-content-center justify-content-md-start mb-3">
-                    <img src="/EscaPinas/frontend/assets/images/Logo 1.png" height="150"  class="d-inline-block align-text-top">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5 text-center">
+                <div class="mb-3">
+                    <img src="/EscaPinas/frontend/assets/images/Logo 1.png" height="150" class="d-inline-block align-text-top">
                 </div>
                 <p class="small text-light">
-                EscaPinas is your gateway to affordable tours across the Philippines. Explore, enjoy, and create unforgettable memories with every trip.</p>         
-                <div class="mt-4">
-                    <a href="#" class="text-light me-3 fs-5"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="text-white me-3 fs-5"><i class="bi bi-instagram"></i></a>
-                    <a href="#" class="text-white me-3 fs-5"><i class="bi bi-twitter-x"></i></a>
-                </div>
+                    EscaPinas is your gateway to affordable tours across the Philippines. Explore, enjoy, and create unforgettable memories with every trip.
+                </p>
             </div>
 
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5 align-items-center">
@@ -32,7 +28,9 @@
                         <i class="bi bi-house-door me-3 fs-5"></i> Tanauan City, Batangas, PH
                     </p>
                     <p class="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
-                        <i class="bi bi-envelope me-3 fs-5"></i> escapinas@test.com
+                        <a href="mailto:escapinas26@gmail.com" class="text-light text-decoration-none">
+                            <i class="bi bi-envelope me-3 fs-5"></i> escapinas26@gmail.com
+                        </a>
                     </p>
                     <p class="d-flex align-items-center justify-content-center justify-content-md-start mb-2">
                         <i class="bi bi-telephone me-3 fs-5"></i> +63 966 693 5239
